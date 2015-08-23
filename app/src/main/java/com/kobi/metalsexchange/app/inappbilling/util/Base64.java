d11de9e80c5e192af6e823a508500d230e939c6c
kobi <kobi.sadoun@gmail.com>
2015-08-23 12:05:41 +0300

    test

--- app/src/main/java/com/kobi/metalsexchange/app/inappbilling/util/Base64.java
+++ app/src/main/java/com/kobi/metalsexchange/app/inappbilling/util/Base64.java
@@ -14,22 +14,6 @@
 
 package com.kobi.metalsexchange.app.inappbilling.util;
 
-// This code was converted from code at http://iharder.sourceforge.net/base64/
-// Lots of extraneous features were removed.
-/* The original code said:
- * <p>
- * I am placing this code in the Public Domain. Do with it as you will.
- * This software comes with no guarantees or warranties but with
- * plenty of well-wishing instead!
- * Please visit
- * <a href="http://iharder.net/xmlizable">http://iharder.net/xmlizable</a>
- * periodically to check for updates or to contribute improvements.
- * </p>
- *
- * @author Robert Harder
- * @author rharder@usa.net
- * @version 1.3
- */
 
 /**
  * Base64 converter class. This code is not a complete MIME encoder;
